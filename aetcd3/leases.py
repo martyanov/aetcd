b66b511d7f2c28377f9e341e4f9fceb32c4e4778
Andrey Martyanov <andrey@martyanov.com>
2021-04-27 19:39:24 +0600

    Refactor and document lease

--- aetcd3/leases.py
+++ aetcd3/leases.py
@@ -1,36 +1,36 @@
 class Lease(object):
-    """
-    A lease.
+    """A lease.
 
-    :ivar id: ID of the lease
-    :ivar ttl: time to live for this lease
+    :param id: ID of the lease
+    :param ttl: time to live for this lease
+    :param etcd_client: Instance of :class:`aetcd3.Etcd3Client`
     """
 
-    def __init__(self, lease_id, ttl, etcd_client=None):
+    def __init__(self, lease_id, ttl, etcd_client):
         self.id = lease_id
         self.ttl = ttl
 
-        self.etcd_client = etcd_client
+        self._etcd_client = etcd_client
 
     async def _get_lease_info(self, *, keys=True):
-        return await self.etcd_client.get_lease_info(self.id, keys=keys)
+        return await self._etcd_client.get_lease_info(self.id, keys=keys)
 
     async def revoke(self):
         """Revoke this lease."""
-        await self.etcd_client.revoke_lease(self.id)
+        await self._etcd_client.revoke_lease(self.id)
 
     async def refresh(self):
         """Refresh the time to live for this lease."""
-        return await self.etcd_client.refresh_lease(self.id)
+        return await self._etcd_client.refresh_lease(self.id)
 
-    # @property
     async def remaining_ttl(self):
+        """Return remaining time to live for this lease."""
         return (await self._get_lease_info(keys=False)).TTL
 
-    # @property
     async def granted_ttl(self):
+        """Return granted time to live for this lease."""
         return (await self._get_lease_info(keys=False)).grantedTTL
 
-    # @property
     async def keys(self):
+        """Return list of keys associated with this lease."""
         return (await self._get_lease_info()).keys
