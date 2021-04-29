97f3e4688c8d567ea0189b6e17e8f6b7346b518e
Andrey Martyanov <andrey@martyanov.com>
2021-04-29 17:22:32 +0600

    Document exceptions

--- aetcd3/exceptions.py
+++ aetcd3/exceptions.py
@@ -1,30 +1,40 @@
 class Etcd3Exception(Exception):
-    pass
+    """Raises on generic client errors."""
 
 
 class WatchTimedOut(Etcd3Exception):
-    pass
+    """Raised on operation timeouts."""
 
 
 class InternalServerError(Etcd3Exception):
-    pass
+    """Raises on etcd internal server errors."""
 
 
 class ConnectionFailedError(Etcd3Exception):
+    """Raises on etcd server connection errors."""
+
     def __str__(self):
         return 'etcd connection failed'
 
 
 class ConnectionTimeoutError(Etcd3Exception):
+    """Raises on etcd server connection timeout errors."""
+
     def __str__(self):
         return 'etcd connection timeout'
 
 
 class PreconditionFailedError(Etcd3Exception):
-    pass
+    """Raises on etcd server precondition errors."""
 
 
 class RevisionCompactedError(Etcd3Exception):
+    """Raises when requested and previous revisions were already compacted.
+
+    :param compacted_revision: Revision bellow values were compacted
+    :type compacted_revision: int
+    """
+
     def __init__(self, compacted_revision):
         self.compacted_revision = compacted_revision
         super(RevisionCompactedError, self).__init__()
