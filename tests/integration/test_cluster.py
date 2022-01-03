6324d65ebb4afe42469e3148c08eb2c201b27b41
Andrey Martyanov <andrey@martyanov.com>
2022-01-03 14:43:06 +0600

    Move cluster related tests into a separate test module

---
+++ tests/integration/test_cluster.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_member_list(etcd):
+    assert len([m async for m in etcd.members()]) == 3
+    async for member in etcd.members():
+        assert member.name.startswith('pifpaf')
+        for peer_url in member.peer_urls:
+            assert peer_url.startswith('http://')
+        for client_url in member.client_urls:
+            assert client_url.startswith('http://')
+        assert isinstance(member.id, int) is True
